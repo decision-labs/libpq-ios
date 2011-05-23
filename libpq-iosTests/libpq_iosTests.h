ec37e4bc490b1876da99ada4f2c09fc2f21c5fad
Kashif Rasul <kashif@nomad-labs.com>
2011-05-23 10:56:29 +0200

    Initial Commit

---
+++ libpq-iosTests/libpq_iosTests.h
@@ -0,0 +1,17 @@
+//
+//  libpq_iosTests.h
+//  libpq-iosTests
+//
+//  Created by Kashif Rasul on 5/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface libpq_iosTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ libpq-iosTests/libpq_iosTests.m
@@ -0,0 +1,33 @@
+//
+//  libpq_iosTests.m
+//  libpq-iosTests
+//
+//  Created by Kashif Rasul on 5/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "libpq_iosTests.h"
+
+
+@implementation libpq_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in libpq-iosTests");
+}
+
+@end
