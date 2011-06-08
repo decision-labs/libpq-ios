7d16bfc1b930125e8d52630d4c9f9db33c161e1c
Kashif Rasul <kashif@nomad-labs.com>
2011-06-08 11:27:03 +0200

    Fixed test for transaction.

--- libpq-iosTests/libpq_iosTests.m
+++ libpq-iosTests/libpq_iosTests.m
@@ -8,7 +8,6 @@
 
 #import "libpq_iosTests.h"
 
-
 @implementation libpq_iosTests
 
 - (void)setUp
@@ -38,13 +37,45 @@
 - (void)testTransaction
 {
     NSLog(@"%@ start", self.name);
-    res = PQexec(conn, "BEGIN");
 
+    res = PQexec(conn, "BEGIN");
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
         NSString *message = [[NSString alloc] initWithUTF8String:PQerrorMessage(conn)];
         NSLog(@"BEGIN command failed: %@", message);
     }
-
+    PQclear(res);
+    res = PQexec(conn, "END");
     PQclear(res);
 }
+
+- (void)testFetch
+{
+    NSLog(@"%@ start", self.name);
+
+    res = PQexec(conn, "BEGIN");
+    PQclear(res);
+
+    res = PQexec(conn, "DECLARE myportal CURSOR FOR select * from pg_database");
+    if (PQresultStatus(res) != PGRES_COMMAND_OK)
+    {
+        NSString *message = [[NSString alloc] initWithUTF8String:PQerrorMessage(conn)];
+        NSLog(@"DECLARE CURSOR failed: %@", message);
+    }
+    PQclear(res);
+
+    res = PQexec(conn, "FETCH ALL in myportal");
+    if (PQresultStatus(res) != PGRES_TUPLES_OK)
+    {
+        NSString *message = [[NSString alloc] initWithUTF8String:PQerrorMessage(conn)];
+        NSLog(@"FETCH ALL failed: %@", message);
+    }
+    PQclear(res);
+
+    res = PQexec(conn, "CLOSE myportal");
+    PQclear(res);
+
+    res = PQexec(conn, "END");
+    PQclear(res);
+}
+
 @end
